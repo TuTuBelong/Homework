a962d7e78eb7a66a049bb97e5fa16312434a0439
zl <1276521436@qq.com>
2022-02-23 11:53:46 +0800

    添加项目文件。

---
+++ App218/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.IO;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace App218
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double a = 0;
+            double b = 0;
+            double c = 0;
+            double d = 0;   
+            Console.WriteLine("请输入number1的值：");
+            a = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("请输入number2的值：");
+            b = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("请输入进位的值：");
+            c = Convert.ToDouble(Console.ReadLine());
+            d = a + b + c;
+            Console.WriteLine(d);
+            Console.WriteLine($"a+b+c: {a + b + c}");
+        }
+    }
+
+}
---
+++ WindowsFormsApp/Form1.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            double a =Convert.ToDouble(textBox1.Text);
+            double b =Convert.ToDouble(textBox2.Text);
+            double c =a+b;
+            textBox3.Text = Convert.ToString(c);
+        }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void comboBox2_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox3_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
