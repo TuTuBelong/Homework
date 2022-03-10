4cc8c22b7162c458f523336368319429ee61fa8e
zl <1276521436@qq.com>
2022-03-10 21:30:33 +0800

    zl

---
+++ homework34/Shape/Program.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Shape
+{
+    public class Square
+    {
+        public double side;
+        public Square(double side)
+        {
+            this.side = side;
+        }
+        public Boolean isSquare()
+        {
+            if(side != 0)
+                return true;
+            else
+                return false;
+                }
+        public double getArea()
+        {
+            return side*side;
+        }
+    }
+    public class Rectangcle
+    {
+        public double length;
+        public double width;
+        public Rectangcle(double length,double width)
+        {
+            this.length = length;
+            this.width = width;
+        }
+        public Boolean isRectangcle()
+        {
+            if (length != width&&length!=0&&width!=0)
+            {
+                return true;
+            }
+            else
+                return false;
+        }
+        public double getArea()
+        {
+            return length * width;
+        }
+    }
+    public class Delta
+    {
+        public double a;
+        public double b;
+        public double c;
+        public Delta(double a,double b,double c)
+        {
+            this.a = a;
+            this.b = b;
+            this.c = c;
+        }
+        public Boolean isDelta()
+        {
+            if (a + b > c && a + c > b && b + c > a)
+            {
+                return true;
+            }
+            else
+                return false;
+        }
+        public double getArea()
+        {
+            double p = (a + b + c) / 2;
+            double s= Math.Sqrt(p*(p-a)*(p-b)*(p-c));
+            return s;
+        }
+    }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Square square1 = new Square(6);
+            if(square1.isSquare()==true)
+            Console.WriteLine(square1.getArea());
+            Rectangcle rectangcle1=new Rectangcle(6,8);
+            if(rectangcle1.isRectangcle()==true)
+            Console.WriteLine(rectangcle1.getArea());
+            Delta delta1 = new Delta(3, 5, 7);
+            if(delta1.isDelta()==true)  
+            Console.WriteLine(delta1.getArea());
+            Console.ReadKey();  
+        }
+    }
+}
---
+++ homework34/factory/Program.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace factory
+{
+    public abstract class Shape
+    {
+        public double a;
+        public double b;
+        public double c;
+        public abstract double getArea();
+       
+    }
+    class Square : Shape
+    {
+        public override double getArea()
+        {
+            Console.Write("正方形的面积是：");
+            return a * a;
+        }
+    }
+    class TanRectangcle : Shape
+    {
+        public override double getArea()
+        {
+            Console.Write("长方形的面积是：");
+            return a * b;
+        }
+    }
+    class Delta : Shape
+    {
+        public override double getArea()
+        {
+            double p = (a + b + c) / 2;
+            Console.Write("三角形的面积是：");
+            return Math.Sqrt(p*(p-a)*(p-b)*(p-c));
+        }
+    }
+    public class shapeFactory
+    {
+        public static Shape createShape(string s)
+        {
+            Shape shape = null;
+            if (s == "长方形")
+                shape = new TanRectangcle();
+            else if (s == "正方形")
+                shape = new Square();
+            else if (s == "三角形")
+                shape = new Delta();
+            else
+                throw new ArgumentException("输入有误");
+            return shape;
+        }
+    }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("请输入形状名称：");
+            string s =Console.ReadLine();
+            Shape shape =shapeFactory.createShape(s);// 创建对象
+            shape.a = 10;
+            shape.b = 5;
+            shape.c = 10;   
+            Console.WriteLine(shape.getArea());
+            Console.ReadKey();
+        }
+    }
+}
