37455e196a56be2968db848df8ef65c559a86ef7
zl <1276521436@qq.com>
2022-03-17 23:36:53 +0800

    [[

---
+++ homework4/clock/Program.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Threading;
+namespace clock
+{
+    public class Clock
+    {
+        public int Hour{get;set;}
+        public int Minute{get;set;}
+        public int Second { get;set;}
+        private int alarmHour;
+        private int alarmMinute;
+        private int alarmSecond;
+        public void setAlarmTime(int h,int m,int s)
+        {
+            alarmHour = h;
+            alarmMinute = m;
+            alarmSecond = s;
+        }
+        //创建委托
+        public delegate void ClockHander(int i,int j,int k );
+        //public event ClockHander Tick;
+        //public event ClockHander Alarm;
+        public void tick(int i, int j, int k)
+        {
+            Console.WriteLine("tick~" + i + ":" + j + ":" + k);
+        }
+        public void alarm(int i, int j, int k)
+        {
+            Console.WriteLine("alarm!" + i + ":" + j + ":" + k);
+        }
+        public void run()
+        {
+            Hour = Minute =Second= 0;
+            for(int i = 0; i < 23; i++)
+            {
+                Hour = i;
+                for(int j = 0; j < 59; j++)
+                {
+                    Minute = j;
+                    for(int k = 0; k < 59; k++)
+                    {
+                        Second = k;
+                        Thread.Sleep(1000);
+                        if (Hour == alarmHour && Minute == alarmMinute && Second == alarmSecond)
+                        {
+                            alarm(i, j, k);
+                        }
+                        else
+                        {
+                            tick(i, j, k);
+                        }
+                    }
+                }
+            }
+        }
+        
+    }
+    //public class ClockEvent
+    //{
+    //    public void tick(int i, int j, int k)
+    //    {
+    //        Console.WriteLine("tick~" + i + ":" + j + ":" + k);
+    //    }
+    //    public void alarm(int i, int j, int k)
+    //    {
+    //        Console.WriteLine("alarm!" + i + ":" + j + ":" + k);
+    //    }
+    //}
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+             Clock clock = new Clock();
+            clock.setAlarmTime(0, 0, 10);
+             //ClockEvent a = new ClockEvent();
+             //clock.Alarm += a.alarm;
+             //clock.Tick += a.alarm;
+             clock.run();
+        }
+    }
+}
---
+++ homework4/generric/Program.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace generric
+{
+    public class Node<T>
+    {
+        public  Node<T> Next{ get; set; }
+        public T Data { get; set; }
+        public Node(T t)
+        {
+            Next = null;
+            Data = t;
+        }
+    }
+    public class GenericList<T>
+    {
+        private Node<T> head;
+        private Node<T> tail;   
+        public GenericList()
+        {
+            head = tail = null;
+        }
+        public Node<T> Head
+        {
+            get => head;
+        }
+        public void add(T t)
+        {
+            Node<T> n = new Node<T>(t);
+            if(tail == null)
+            {
+                head=tail = n;
+            }
+            else
+            {
+                tail.Next = n;
+                tail = n;
+            }
+            
+        }
+        public void printNode(Action<T>action)
+        {
+            while(head != null)
+            {
+                action(head.Data);
+                head = head.Next;
+            }
+        }
+    }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+           GenericList<int> list = new GenericList<int>();
+            for(int i = 0; i < 10; i++)
+            {
+                list.add(i);   
+            }
+            int sum=0;
+            int min=0;
+            int max=0;
+            list.printNode((m) =>
+            {
+                Console.WriteLine(m);
+                sum += m;
+                min = Math.Min(min, m);
+                max = Math.Max(max, m);
+            });
+            Console.WriteLine("最大值" + max);
+            Console.WriteLine("最小值" + min);
+            Console.WriteLine("总和" + sum);
+            Console.ReadKey();
+        }
+    }
+}
